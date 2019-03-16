24a0151d11435549ba24c551889fdc87759bbc8f
yasuda <tasssra@gmail.com>
2019-03-16 15:34:44 +0900

    rails g model category name:string & db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_16_044221) do
+ActiveRecord::Schema.define(version: 2019_03_16_063345) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -38,6 +38,12 @@ ActiveRecord::Schema.define(version: 2019_03_16_044221) do
     t.integer "price", null: false
     t.date "publish_date", null: false
     t.text "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
