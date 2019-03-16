997ae6f53695674b0c9ee3ff9cdaeea3708bee7e
yasuda <tasssra@gmail.com>
2019-03-16 11:15:50 +0900

    書籍登録機能を実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,24 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to @book, notice: "書籍を登録しました"
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :price, :publish_date, :description)
+  end
 end
