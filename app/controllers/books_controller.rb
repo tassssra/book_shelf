d678caf73e0d3c2d7e8e2a8ee3c94aa02be6a219
yasuda <tasssra@gmail.com>
2019-03-16 14:58:27 +0900

    編集機能を実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,9 +20,34 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to @book, notice: "書籍を更新しました。"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path, notice: "書籍を削除しました。"
+  end
+
   private
 
   def book_params
-    params.require(:book).permit(:title, :price, :publish_date, :description)
+    params.require(:book).permit(
+                          :title,
+                          :price,
+                          :publish_date,
+                          :description,
+                          :new_image
+                          )
   end
 end
